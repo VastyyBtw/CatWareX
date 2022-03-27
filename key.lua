7c9387df2b50e4262e5c73eb3d8eb8db5573b748
VastyyBtw <97897529+VastyyBtw@users.noreply.github.com>
2022-03-28 00:20:15 +1030

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,3 +1,3 @@
-if _G.Key == "test" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/VastyyBtw/CatWare/main/loader.lua", true))()
+if _G.Key == game:HttpGet("https://elysiann.000webhostapp.com/api/v1/keys/currentKey/" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VastyyBtw/CatWare/main/loader.lua", true))()
 end
